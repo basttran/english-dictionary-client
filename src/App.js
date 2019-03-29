bfd60b39096bac932e5fd1bdb5ff329a4d5f7aec
Bastien Tran <bastien.tran@gmail.com>
2019-03-29 11:01:25 +0100

    Component files created and imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import { Switch, Route } from "react-router-dom";
+import EntriesSubmission from "./components/EntriesSubmission.js";
+import SearchBar from "./components/SearchBar.js";
+import SearchResult from "./components/SearchResult.js";
+
+
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      // App.js will centralize all child components' data (dictionary, query, result)
+    this.state = {
+      // Dictionary is provided as a JSON by the user via <EntriesSubmission/>
+      dictionary: {},
+      // result will be passed to <SearchResult/> as prop
+      // genricOnChange will update query and be passed to <SearchBar/> as a prop
+      query: "",
+      result: { 
+        // default 'result' that will be displayed before the first term submission
+        term: "Browse our dictionary", 
+        definition: "Type your query string in the above search field." 
+      }
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/* Maybe navlinks later here later for switching between dictionary submission and dictionary search, hence with a <Switch> component below */}
         </header>
+          <EntriesSubmission/>
+          <SearchBar/>
+          <SearchResult/>
       </div>
     );
   }
